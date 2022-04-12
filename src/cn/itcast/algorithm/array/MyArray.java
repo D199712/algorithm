3506571be266c7d5c55ab4623fb94b5f92f349e5
我的电脑 <2075944260@qq.com>
2022-04-13 00:39:30 +0800

    冒泡排序

--- src/cn/itcast/algorithm/array/MyArray.java
+++ src/cn/itcast/algorithm/array/MyArray.java
@@ -32,13 +32,54 @@ public class MyArray {
             throw new IndexOutOfBoundsException("超出数组实际元素范围！");
         }
 
+        if (size >= array.length){
+            resize();
+        }
+
+        System.out.println("初始SIZE："+size);
         //从右向左循环，将元素逐个向右移一位
-        for (int i = size-1; i>=i;i--){
-            array[i+1] = array[index];
+        for (int i = size-1; i>=index; i--){
+            System.out.println("本次循环元素下标："+i);
+            array[i+1] = array[i];
         }
         //腾出的位置放入新元素
         array[index] = element;
         size++;
+        System.out.println("最终SIZE:"+size);
+        System.out.println("!!!!!!!!!!!!!!!");
+    }
+
+    /**
+     * 数组扩容
+     */
+    public void resize(){
+        int [] arrayNew = new int[array.length*2];
+        //从旧数组复制到新数组
+        System.arraycopy(array,0,arrayNew,0,array.length);
+        array = arrayNew;
+        System.out.println("数组扩容");
+    }
+
+    /**
+     * 数组删除元素
+     * @param index
+     * @return
+     * @throws Exception
+     */
+    public int delete(int index) throws Exception{
+        //判断访问下标是否超出范围
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("超出数组实际元素范围！");
+        }
+
+        int deleteElement = array[index];
+        //从左向右循环，将元素逐个向左挪移一位
+        for (int i = index; i<size;i++){
+            array[i] = array[i+1];
+        }
+        size--;
+        System.out.println("删除节点："+index+",节点值："+deleteElement);
+        return deleteElement;
     }
 
     /**
@@ -46,16 +87,19 @@ public class MyArray {
      */
     public void output(){
         for (int i=0; i <= size; i++){
-            System.out.println(array[i]);
+            System.out.print(array[i]);
         }
     }
 
     public static void main(String[] args) throws Exception{
-        MyArray myArray = new MyArray(10);
+        MyArray myArray = new MyArray(4);
         myArray.insert(3,0);
+        myArray.insert(7,1);
         myArray.insert(9,2);
         myArray.insert(5,3);
-        myArray.insert(7,3);
+        myArray.insert(4,1);
+//        myArray.insert(4,7);
+        myArray.delete(3);
         myArray.output();
     }
 }
---
+++ src/cn/itcast/algorithm/sort/BubbleSort.java
@@ -0,0 +1,51 @@
+package cn.itcast.algorithm.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    /**
+     * 冒泡排序：双层for循环，外层控制回合，内层控制元素比较和交换
+     * 有序标记：在本回合排序中如果元素未发生交换说明之后的数列已经有序，跳出外循环；
+     *          如果元素发生交换说明之后得到数列无序，继续循环
+     *
+     * @param array
+     */
+    public static void sort(int array[]){
+        //中间变量tmp
+        int tmp = 0;
+        //记录最后一次交换的位置
+        int lastExchangeIndex = 0;
+        //无序数列边界，每次比较只需比到这里为止
+        int sortBorder = array.length -1;
+
+        for (int i = 0; i < array.length; i++){
+            //有序标记，每一轮开始前初始值为true
+            boolean isSorted = true;
+
+            for (int j = 0; j< sortBorder;j++){
+
+                if(array[j] > array[j+1]){
+                    tmp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = tmp;
+
+                    //当元素进行交换，说明不是有序的，标记为false
+                    isSorted = false;
+                    //把无序数列的边界更新为最后一次交换元素的位置
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            System.out.println("第"+i+"回合");
+            if (isSorted){
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3,4,2,1,5,6,7,8};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
