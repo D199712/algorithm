f52a99ad2ec8b0e8a60f15f38d583544e173d0d5
我的电脑 <2075944260@qq.com>
2022-04-13 23:57:10 +0800

    鸡尾酒排序

---
+++ src/cn/itcast/algorithm/sort/Selection.java
@@ -0,0 +1,14 @@
+package cn.itcast.algorithm.sort;
+
+public class Selection {
+
+    public static void sort(Comparable[] a){
+        for (int i=0;i<a.length-2;i++){
+            //定义一个变量，记录最小元素所在索引，默认为参与选择排序的第一个元素所在位置
+            int minIndex = i;
+            for (int j=i+1;j<a.length;j++){
+
+            }
+        }
+    }
+}
