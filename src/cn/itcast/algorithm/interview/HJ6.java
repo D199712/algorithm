86e4c404058b9e161f777a90854e51eb7ec91bd4
DJ <2075944260@qq.com>
2022-05-05 00:02:05 +0800

    feat:HJ6

--- src/cn/itcast/algorithm/interview/HJ6.java
+++ src/cn/itcast/algorithm/interview/HJ6.java
@@ -1,5 +1,9 @@
 package cn.itcast.algorithm.interview;
 
+import cn.itcast.algorithm.sort.Student;
+
+import java.util.Scanner;
+
 /**
  描述
  功能:输入一个正整数，按照从小到大的顺序输出它的所有质因子（重复的也要列举）（如180的质因子为2 2 3 3 5 ）
@@ -16,9 +20,23 @@ package cn.itcast.algorithm.interview;
 
  输出：
  2 2 3 3 5
+
+ 核心思路： 正如我们判断数 num 是不是质数时，没必要从 2 一直尝试到 num 一样，此题中的大循环也大可不必写一个到 num 的循环，写到 根号num s
+ 即可，如果此时数字还没有除数，则可判定其本身是一个质数，没有再除下去的必要了，直接打印其本身即可：
  **/
 public class HJ6 {
     public static void main(String[] args) {
 
-    }
-}
+        Scanner sc = new Scanner(System.in);
+        long input = sc.nextLong();
+        long s = (long)(Math.sqrt(input));
+        for(int i = 2; i <= s; i++){
+            if(input % i == 0){
+
+                System.out.print(i+" ");
+                input /= i;
+                i--;//为了重复质数
+            }
+        }
+        System.out.print(input ==1 ? "": input + " ");
+    }}
