cf5417d9d9db0f6764b9119b42bf81ddf9574933
我的电脑 <2075944260@qq.com>
2022-06-07 00:32:24 +0800

    feat:

--- src/cn/itcast/algorithm/sort/BubbleSort1.java
+++ src/cn/itcast/algorithm/sort/BubbleSort1.java
@@ -19,10 +19,8 @@ public class BubbleSort1 {
     public static void sort(int array[]){
         int tmp = 0;
         for (int i = 0; i < array.length -1; i++){
-
-
             for (int j = 0; j< array.length -i-1;j++){
-
+                System.out.println("i:"+i+" j:"+j);
                 if(array[j] > array[j+1]){
                     tmp = array[j+1];
                     array[j+1] = array[j];
