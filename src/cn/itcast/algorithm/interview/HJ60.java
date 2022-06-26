73dddde88041079860712a71a99ef6029760964b
我的电脑 <2075944260@qq.com>
2022-06-26 23:54:49 +0800

    feat:HJ60

--- src/cn/itcast/algorithm/interview/HJ60.java
+++ src/cn/itcast/algorithm/interview/HJ60.java
@@ -1,5 +1,10 @@
 package cn.itcast.algorithm.interview;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
 /**
  * HJ60 查找组成一个偶数最接近的两个素数
  * 描述
@@ -33,4 +38,33 @@ package cn.itcast.algorithm.interview;
  * 循环结束之后得到最小差值和两个目标素数
  */
 public class HJ60 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int min = Integer.MAX_VALUE;
+        int[] result = new int[2];
+        //在2-n/2之间循环,
+        for (int j = 2; j <= n/2; j++){
+            //判断是否满足素数对条件
+            if(isPrime(j) && isPrime(n-j)){
+                //判断差值是否小于上一组
+                if (Math.abs(2*j-n) < min){
+                    result[0] = j;
+                    result[1] = n-j;
+                    min = Math.abs(2*j-n);
+                }
+            }
+        }
+        System.out.println(result[0] + "\n" +result[1]);
+    }
+
+    //因数只有1和自身
+    public static boolean isPrime(int target){
+        for(int i = 2; i < target; i++){
+            if(target%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
