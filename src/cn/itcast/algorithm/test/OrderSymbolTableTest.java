01a206a250fdfe5d7b9b027b336778777a66125e
DJ <2075944260@qq.com>
2022-07-13 20:30:28 +0800

    feat：顺序符号表

---
+++ src/cn/itcast/algorithm/test/OrderSymbolTableTest.java
@@ -0,0 +1,21 @@
+package cn.itcast.algorithm.test;
+
+import cn.itcast.algorithm.Symbol.OrderSymbolTable;
+
+/**有序符号表测试
+ * @Author DJ
+ * @Description //TODO $end$
+ * @Date $time$ $date$
+ * @return $return$
+ */
+public class OrderSymbolTableTest {
+    public static void main(String[] args) {
+        OrderSymbolTable<Integer, String> symbolTable = new OrderSymbolTable<>();
+        symbolTable.put(1,"亚索");
+        symbolTable.put(2,"赵信");
+        symbolTable.put(4,"嘉文");
+        symbolTable.put(6,"盖伦");
+
+        symbolTable.put(3,"永恩");
+    }
+}
