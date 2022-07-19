d47d963a2c15a6808f41fec0866ef7c4edffdc8a
我的电脑 <2075944260@qq.com>
2022-07-20 00:27:35 +0800

    feat:二叉树

--- src/cn/itcast/algorithm/tree/BinaryTree.java
+++ src/cn/itcast/algorithm/tree/BinaryTree.java
@@ -70,6 +70,64 @@ public class BinaryTree<Key extends Comparable<Key>, Value>{
         }
     }
 
+    //删除树中key对应的value
+    public void delete(Key key){
+
+    }
+
+    //删除指定树x中的key对应的value，并返回删除后的新树
+    public Node delete(Node x,Key key){
+        if (x == null){
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp > 0){
+            //新结点的key大于当前结点的key，继续找当前结点的右子结点
+            return delete(x.right,key);
+        }else if (cmp > 0){
+            //新结点的key小于当前结点的key，继续找当前结点的左子结点
+            return delete(x.left,key);
+        }else {
+            //新结点的key等于当前结点的key,当前x就是要删除的结点
+            // 1.如果当前结点的右子树不存在，则直接返回当前结点的左子结点
+            if (x.right == null){
+                return x.left;
+            }
+
+            // 2.如果当前结点的左子树不存在，则直接返回当前结点的右子结点
+            if (x.left == null){
+                return x.right;
+            }
+
+            //3.当前结点的左右子树都存在
+            //3.1找到右子树中最小的结点
+            Node minNode = x.right;
+            while (minNode.left != null){
+                minNode = minNode.left;
+            }
+
+            //3.2删除右子树中最小的结点
+            Node n = x.right;
+            while (n.left != null){
+                //孙节点为空，当前节点左子节点置空，不为空则用左子节点继续循环
+                if (n.left.left == null){
+                    n.left = null;
+                }else {
+                    n = n.left;
+                }
+            }
+
+            //3.3让被删除结点的左子树称为最小结点minNode的左子树，让被删除结点的右子树称为最小结点 minNode的右子树
+            minNode.left = x.left;
+            minNode.right = x.right;
+            //3.4让被删除结点的父节点指向最小结点minNode
+            x = minNode;
+            N--;
+        }
+        return x;
+    }
+
     private class Node {
         //存储键
         public Key key;
