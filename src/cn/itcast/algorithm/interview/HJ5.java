f6cba3460f6147da24ab37c8d2dddc1601cd66b7
DengJie <2075944260@qq.com>
2023-08-15 00:50:05 +0800

    HJ15：进制转换

--- src/cn/itcast/algorithm/interview/HJ5.java
+++ src/cn/itcast/algorithm/interview/HJ5.java
@@ -1,5 +1,9 @@
 package cn.itcast.algorithm.interview;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。
  *
@@ -19,4 +23,21 @@ package cn.itcast.algorithm.interview;
  * 170
  * */
 public class HJ5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        solution1(br);
+
+    }
+
+    /**
+     * 16进制前两位是0x,使用substring去掉前两位，再用Long.parseLong方法进行转换
+     * @param br
+     * @throws IOException
+     */
+    public static void solution1(BufferedReader br) throws IOException {
+        String line = null;
+        while ((line = br.readLine()) != null){
+            System.out.println(Long.parseLong(line.substring(2),16));
+        }
+    }
 }
